836f8852a926916d0d3dffd66bd8db7b342d54c7
Andre <andreleitep@gmail.com>
2026-03-17 12:45:53 -0300

    correções de bugs, implementações de mecânicas

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -4,7 +4,20 @@ function toggleMenu() {
     const menu = document.getElementById("menu-list");
     const toggle = document.querySelector(".menu-toggle");
 
-    menu.classList.toggle("show")
+    if (menu && toggle) {
+        menu.classList.toggle("show")
+        toggle.classList.toggle("active");
+    }
+};
 
-    toggle.classList.toggle("active");
-}
+document.addEventListener("click", function() {
+    const menu = document.getElementById("menu-list");
+    const toggle = document.querySelector(".menu-toggle");
+    const subtoggle = document.querySelector(".expo_submenu");
+
+    if (event.target.closest("#menu-list a")) {
+        menu.classList.remove("show");
+        toggle.classList.remove("active");
+        subtoggle.classList.remove("submenu-open");
+    }
+});
